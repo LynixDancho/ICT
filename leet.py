50945a04d1fc356ec0e90117147f6e385cc42407
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-02 23:40:40 +0100

    r

--- leet.py
+++ leet.py
@@ -2,52 +2,18 @@ from typing import List
 import numpy  as np 
 import math as m
 import matplotlib.pyplot as pl
-import scipy.optimize as sc
-class ListNode:
-    def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = ListNode()
-        tail = head
-        counter=0
-        while l1 and l2 or counter :
-         value1= l1.val if l1 else 0 
-         value2 = l2.val if l2 else 0
-         add  = value1 + value2 + counter
-         counter = add // 10 
-         add = add% 10
-         tail.next = ListNode(add)
-         tail= tail.next
-         l1=l1.next if l1 else l1 
-         l2=l2.next if l2 else l2
+from scipy import stats
 
-         
+from scipy.stats import ecdf
 
-
-        return head.next
-
-
-
-# l1 = [9,9,9,9]
-l1 = ListNode(9)
-l1.next = ListNode(9)
-l1.next.next = ListNode(9)
-l1.next.next.next = ListNode(9)
-
-# l2 = [9,9,9,9,9,9,9]
-l2 = ListNode(9)
-l2.next = ListNode(9)
-l2.next.next = ListNode(9)
-l2.next.next.next = ListNode(9)
-l2.next.next.next.next = ListNode(9)
-l2.next.next.next.next.next = ListNode(9)
-l2.next.next.next.next.next.next = ListNode(9)
-
- 
-v= Solution.addTwoNumbers(None,l1,l2)
-
-while v:
-    print(v.val,'--->')
-    v=v.next
+data =  np.linspace(20, 80, 500)
+poisson= np.arange(0, 15)
+binom = np.arange(0,13)
+mean,std = data.mean() , data.std()
+Cdf_norm=  stats.norm.cdf(14, mean , std)
+#Cdf_poisson = stats.poisson.cdf(poisson)
+#Cdf_binom = stats.binom.cdf(binom)
+pmf_norm=  stats.norm.pdf(data)
+#pmf_poisson = stats.poisson.pmf(poisson)
+#pmf_binom = stats.binom.pmf(binom)
+print(Cdf_norm)
