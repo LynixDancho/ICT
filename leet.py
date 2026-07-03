fc439611c095a27b82d2e77f05c9d228e133d239
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-03 19:29:25 +0100

    fd

--- leet.py
+++ leet.py
@@ -3,17 +3,15 @@ import numpy  as np
 import math as m
 import matplotlib.pyplot as pl
 from scipy import stats
-
-from scipy.stats import ecdf
-
+y_axos = np.linspace(0,1,500)
 data =  np.linspace(20, 80, 500)
 poisson= np.arange(0, 15)
 binom = np.arange(0,13)
 mean,std = data.mean() , data.std()
-Cdf_norm=  stats.norm.cdf(14, mean , std)
+Cdf_norm=  stats.norm.cdf(data, mean , std)
 #Cdf_poisson = stats.poisson.cdf(poisson)
 #Cdf_binom = stats.binom.cdf(binom)
-pmf_norm=  stats.norm.pdf(data)
-#pmf_poisson = stats.poisson.pmf(poisson)
+pmf_norm=  stats.norm.pdf(data,mean,std)
+pmf_poisson = stats.poisson.pmf(5, poisson.mean())
 #pmf_binom = stats.binom.pmf(binom)
-print(Cdf_norm)
+
