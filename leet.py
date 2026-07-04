802b138d9e20d9a81f9dc097b01503739b7c1eed
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-04 15:46:08 +0100

    Update leet.py

--- leet.py
+++ leet.py
@@ -1,17 +1,23 @@
 from typing import List
 import numpy  as np 
 import math as m
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 from scipy import stats
-y_axos = np.linspace(0,1,500)
-data =  np.linspace(20, 80, 500)
-poisson= np.arange(0, 15)
-binom = np.arange(0,13)
-mean,std = data.mean() , data.std()
-Cdf_norm=  stats.norm.cdf(data, mean , std)
-#Cdf_poisson = stats.poisson.cdf(poisson)
-#Cdf_binom = stats.binom.cdf(binom)
-pmf_norm=  stats.norm.pdf(data,mean,std)
-pmf_poisson = stats.poisson.pmf(5, poisson.mean())
-#pmf_binom = stats.binom.pmf(binom)
+def montecarlo(numberofrols):
+    x= np.random.randint(1,7,numberofrols)
+    #y= np.random.randint(1,7,numberofrols)
+     
+    x = np.array(x)
+    sum = np.sum(x)
+    return sum/numberofrolls
 
+
+
+numberofrolls = 10000
+
+counts = montecarlo(numberofrolls)
+counts = np.array(counts)
+
+print(counts)
+
+ 
