3943f4969f9596997ae76b1ede9271d974990c07
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-10 21:54:39 +0100

    Update leet.py

--- leet.py
+++ leet.py
@@ -4,43 +4,27 @@ import math as m
 import matplotlib.pyplot as plt
 from scipy import stats
 import re
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
-        i = 0 
-        j= 0 
-        newmerged=[]
-        mediane= 0 
-        while i <= len(nums1)-1 or j <= len(nums2)-1:
-            if i >= len(nums1) and j < len(nums2):
+def isBadVersion(version: int) -> bool:
+        return version >= 3 
+
+def firstBadVersion( n: int) -> int:
+        
+        l,r=0,n
+        res=0
+        while l<=r :
+            m = l+((r-l)//2)
+            if isBadVersion(m):
+                r= m-1
+                res=m
+                print(res)
                 
-                newmerged.append(nums2[j])
-                j+=1
-                continue
-            if i < len(nums1) and j >= len(nums2):
-                newmerged.append(nums1[i])
-                i+=1   
-                continue             
-            
-            if nums1[i]< nums2[j]:
-                newmerged.append(nums1[i])
-                i+=1
-            else:
-                newmerged.append(nums2[j])
-                j+=1
-        if newmerged: 
-            middle = len(newmerged) //2 
-            mediane =( (newmerged[middle] + newmerged[middle-1])/2) if (len(newmerged) %2 ) == 0 else newmerged[middle]
-
-        return mediane
+            elif not isBadVersion(m):
+                l = m +1
+        return res
 
 
+x = firstBadVersion(5)
+lt = list(range(1,5+1))
 
-
-
-test = []
-gg=[]
-x = Solution.findMedianSortedArrays(None,test,gg)
-
- 
 print(x)
