49bc748fd367350f7f41877006461c402a8bb3b5
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-13 14:30:22 +0100

    Update leet.py

--- leet.py
+++ leet.py
@@ -4,34 +4,44 @@ import math as m
 import matplotlib.pyplot as plt
 from scipy import stats
 import time
-def Partition( array , left:int , right:int  ):
-        i = left
-        j= right-1
-        pivot= array[right]
-        while i<j:
-                print(j )
-                while array[i]<pivot and i<right:
-                        i+=1
-                while array[j]>= pivot and j>left:
+from collections import defaultdict
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        compar=[]
+        target=0
+
+        nums.sort()
+        for target , value in enumerate(nums):
+            print(nums)
+
+            if value > 0 :
+                break
+            if target> 0 and nums[target] == nums[target-1]:
+                continue
+            i=target+1
+            j = len(nums)-1 
+            while i < j :
+                if nums[i]+nums[j ]+value ==0:
+                    print(i , j, target)
+                    compar.append([nums[i],nums[j ],value ])
+                    j-=1
+                    while nums[j] == nums[j+1] and j< len(nums)-2 and i<j:
                         j-=1
-                        
-                if i<j:
-                        array[i], array[j]= array[j],array[i]
+                elif nums[i]+nums[j ]+value>0:
+                    j-=1
+                else:
+                    i+=1
+        return compar
+                
 
-        if array[i] >pivot:
-                array[i], array[right]= array[right], array[i]
-        return i 
- 
-def quicksort(array, left:int , right:int ):
-    if left<right:
-        partition = Partition(array, left,right)
-        quicksort(array ,left ,partition-1)
-        quicksort(array, partition+1, right)
+            
 
 
-start= time.perf_counter()
+        
 
-x = [1,2,2,2,2,2,2,2,2,5]
-quicksort(x,0,len(x)-1)
-end = time.perf_counter()
-print(f"Time: {end - start:.6f} seconds")
+nums1 =[1,2,0,1,0,0,0,0]
+
+
+x = Solution.threeSum(None,nums1)
+
+print(x)
