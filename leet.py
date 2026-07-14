bc251b9000a1bdef88bdd79ec228d2654ece0049
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-14 23:50:17 +0100

    Update leet.py

--- leet.py
+++ leet.py
@@ -4,24 +4,28 @@ import math as m
 import matplotlib.pyplot as plt
 from scipy import stats
 import time
+from collections import deque
 from collections import OrderedDict
-class LRUCache:
-    def __init__(self,capacity):
-        self.capacity= capacity
+
+class LRUcach:
+    def __init__(self,capacity ):
+        self.capacity = capacity
         self.cache = OrderedDict()
-    def get(self,key):
+    def get(self,key ):
         if key not in self.cache:
             return -1
         self.cache.move_to_end(key)
+        return self.cache[key]
     def put(self,key,value):
-        if key  in self.cache:
+        if key in self.cache:
             self.cache.move_to_end(key)
-        self.cache[key] = value
-        if len(self.cache)> self.capacity:
-            self.cache.popitem(last=False)
-        
-        
-        
+        self.cache[key]= value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(False)
 
 
-print(nums)
+            
+        
+
+ye = deque()
+print()
