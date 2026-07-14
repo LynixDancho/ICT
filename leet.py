224b855995a0a7cbc3c2f76310c308c9cd1f7725
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-14 19:07:15 +0100

    Update leet.py

--- leet.py
+++ leet.py
@@ -5,43 +5,36 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import time
 from collections import defaultdict
-class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        compar=[]
-        target=0
+def heapify(array, i):
+    left = 2*i +1 
+    right = 2*i+2
+    if left <  len(array) and array[left]< array[i]:
+        max = left
+    else :
+        max= i     
+    if right <  len(array) and array[right] < array[max]:
+        max = right
+    if ( max !=i ):
+        array[i],array[max] = array[max],array[i]
+        heapify(array,max)
 
-        nums.sort()
-        for target , value in enumerate(nums):
-            print(nums)
-
-            if value > 0 :
-                break
-            if target> 0 and nums[target] == nums[target-1]:
-                continue
-            i=target+1
-            j = len(nums)-1 
-            while i < j :
-                if nums[i]+nums[j ]+value ==0:
-                    print(i , j, target)
-                    compar.append([nums[i],nums[j ],value ])
-                    j-=1
-                    while nums[j] == nums[j+1] and j< len(nums)-2 and i<j:
-                        j-=1
-                elif nums[i]+nums[j ]+value>0:
-                    j-=1
-                else:
-                    i+=1
-        return compar
-                
-
-            
+def build_max_heap(array):
+    
+    for i in range(len(array)//2,-1,-1):
+        heapify(array,i)
 
 
-        
+def heapsort(array):
+    build_max_heap(array)
+    for i in range(len(array)-1,-1,-1):
+        print(i)
+        array[i],array[0]= array[0],array[i]
+        heapify(array,i)
+    print(array)
 
-nums1 =[1,2,0,1,0,0,0,0]
+
+nums = [14,55,66,34,22,12,33,44]
+heapsort(nums)
 
 
-x = Solution.threeSum(None,nums1)
-
-print(x)
+print(nums)
