26adc3e1227865efa33450c6c1c0ee766beffaf4
LynixDancho <96496354+LynixDancho@users.noreply.github.com>
2026-07-18 20:09:55 +0100

    f

---
+++ Learn.cpp
@@ -0,0 +1,13 @@
+// This program prints the C++ language standard your compiler is currently using
+// Freely redistributable, courtesy of learncpp.com (https://www.learncpp.com/cpp-tutorial/what-language-standard-is-my-compiler-using/)
+
+#include <iostream>
+
+ 
+
+int main()
+{
+    
+ std::cout << sizeof(long ) << '\n';
+
+     return 0; }  
